0e845fb3644ca1a92f012c4362ebce47b2e141a4
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-12 21:59:49 +0300

    Kdv oranı 0-1000 arasında 0,18, Kdv oranı 1000 > ise 0,08 olarak alındı

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //değişkenleri olustur
-        double tutar, kdvtutar, kdvorani = 0.18, kdvlitutar, kdvsiztutar;
+        double tutar, kdvtutar, kdvorani, kdvlitutar, kdvsiztutar;
 
         Scanner inp = new Scanner(System.in);
         System.out.print("Fiyat Giriniz: ");
         tutar = inp.nextInt();
 
-        kdvtutar = tutar * kdvorani;
+        if (tutar<1000) {
+            kdvorani = 0.18;
+        } else
+            kdvorani = 0.08;
+
+        kdvtutar = tutar * (kdvorani);
         kdvlitutar = tutar + kdvtutar;
         kdvsiztutar =tutar;
         kdvorani = tutar * kdvorani;
